9c317e9d68457ac911aff8f97ecfb9e87a6d9d99
eekoka <66888386+eekoka@users.noreply.github.com>
2026-05-13 14:45:51 +0200

    recursive factorials task done

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,13 @@
-const factorial = function() {
+const factorial = function(x) {
+  if (typeof x !== 'number' || x < 0 || !Number.isInteger(x)) {
+    return undefined;
+  }
 
+  if (x == 0) {
+    return 1;
+  } else {
+    return x * factorial(x - 1);
+  }
 };
 
 // Do not edit below this line
