27f39c0dd9e2c36c9c8b4cf6c7880489f4dd60ed
eekoka <66888386+eekoka@users.noreply.github.com>
2026-05-18 22:26:35 +0200

    adds headNode and tailNode methods

--- linked-list/linked-list.js
+++ linked-list/linked-list.js
@@ -6,6 +6,32 @@ class LinkedList {
         this.length = 0;
     }
 
+    size = function () {
+        return this.length;
+    }
+
+    headNode = function () {
+        if (!this.head) {
+            return undefined
+        };
+        return this.head.value;
+    }
+
+    tailNode = function () {
+        if (!this.head) {
+            return undefined
+        };
+
+        let current = this.head;
+        
+        // Loop until current.next is null (the last node)
+        while (current.next) {
+            current = current.next;
+        }
+        // Returns the full node object
+        return current.value;
+    }
+
     append = function (value) {
         const newNode = new Node(value);
         // If list is empty, make it the head
@@ -22,6 +48,20 @@ class LinkedList {
         }
         this.length++;
     };
+
+    prepend = function (value) {
+        const newNode = new Node(value);
+        // If list is empty, make it the head
+        if (!this.head) {
+            this.head = newNode;            
+        }
+        else {
+            // add node as new head
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+    }
 
     remove = function (value) {
         previousNode = null;
@@ -70,4 +110,9 @@ const urList = new LinkedList();
 urList.append("X");
 urList.append("Y");
 urList.append("Z");
+urList.prepend("A");
 urList.printList();
+
+console.log(urList.size());
+console.log(urList.headNode());
+console.log(urList.tailNode());
